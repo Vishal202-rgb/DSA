b120f41b37b36ce3cb858ae295c0d7ac8a370812
vishal kumar <vishalkumar150206@gmail.com>
2026-02-03 16:49:48 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (66.82%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -0,0 +1,64 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) 
+  {
+		// if list1 happen to be NULL
+		// we will simply return list2.
+		if(l1 == NULL)
+        {
+			return l2;
+		}
+		
+		// if list2 happen to be NULL
+		// we will simply return list1.
+		if(l2 == NULL)
+        {
+			return l1;
+		} 
+		
+		// if value pointend by l1 pointer is less than equal to value pointed by l2 pointer
+		// we wall call recursively l1 -> next and whole l2 list.
+		if(l1 -> val <= l2 -> val)
+        {
+			l1 -> next = mergeTwoLists(l1 -> next, l2);
+			return l1;
+		}
+		// we will call recursive l1 whole list and l2 -> next
+		else
+        {
+			l2 -> next = mergeTwoLists(l1, l2 -> next);
+			return l2;            
+		}
+	}
+    ListNode*partitionAndMerge(int start,int end,vector<ListNode*>& lists){
+        if(start>end) {
+            return NULL;
+        }
+        if(start==end){
+            return lists[start];
+        }
+          int mid = start + (end-start)/2;
+        
+        ListNode* l1 = partitionAndMerge(start, mid, lists);
+        ListNode* l2 = partitionAndMerge(mid+1, end, lists);
+        
+        return mergeTwoLists(l1, l2);
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int k=lists.size();
+         if(k== 0)
+            return NULL;
+        
+        return partitionAndMerge(0, k-1, lists);
+    }
+};
