ffa7d3710aa44aa6c894bd079fcc20e0c8983041
vishal kumar <vishalkumar150206@gmail.com>
2026-02-17 15:20:20 +0530

    Time: 4 ms (59.18%) | Memory: 22.7 MB (60.71%) - LeetSync

---
+++ 3252-count-the-number-of-incremovable-subarrays-i/count-the-number-of-incremovable-subarrays-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int incremovableSubarrayCount(vector<int>& nums) {
+        int n=nums.size();
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                int last = -1, flag = 1;
+                    for(int k = 0; k < n; k++){
+                        if(k >= i && k <= j) continue;
+                            if(last >= nums[k]){ flag = 0; break; }
+                                 last = nums[k];
+            }
+            if(flag) ans++;
+        }
+    }
+    return ans;
+}
+};
