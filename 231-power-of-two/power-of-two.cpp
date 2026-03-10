fbc1e79a04278b9610dac80f2ba994465939845e
vishal kumar <vishalkumar150206@gmail.com>
2026-03-10 15:56:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (12.70%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        for(int i=0;i<=30;i++){
-            int ans=pow(2,i);
-            if(ans==n){
-                return true;
-            }
-        }
-        return false;
+        // for(int i=0;i<=30;i++){
+        //     int ans=pow(2,i);
+        //     if(ans==n){
+        //         return true;
+        //     }
+        // }
+        // return false;
+        if(n==1) return true;
+        if(n<=0||n%2!=0) return false;
+
+        return isPowerOfTwo(n/2);
     }
 };
