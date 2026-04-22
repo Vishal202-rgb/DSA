af94c90439be778d200b70f895c3fdbcfb0f25ca
vishal kumar <vishalkumar150206@gmail.com>
2026-04-22 17:25:26 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (7.93%) - LeetSync

--- 2844-sum-of-squares-of-special-elements/sum-of-squares-of-special-elements.cpp
+++ 2844-sum-of-squares-of-special-elements/sum-of-squares-of-special-elements.cpp
@@ -5,7 +5,7 @@ public:
         int res=0;
         for(int i=0;i<n;i++){
             if(n%(i+1)==0){
-                res+=pow(nums[i],2);
+                res+=(nums[i]*nums[i]);
             }
         }
         return res;
