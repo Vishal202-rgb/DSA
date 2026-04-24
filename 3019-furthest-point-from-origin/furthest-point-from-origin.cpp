34293414d8dceb88467d92ff761f51e7405be5e1
vishal kumar <vishalkumar150206@gmail.com>
2026-04-24 07:30:07 +0530

    Time: 1 ms (12.55%) | Memory: 9.1 MB (65.27%) - LeetSync

--- 3019-furthest-point-from-origin/furthest-point-from-origin.cpp
+++ 3019-furthest-point-from-origin/furthest-point-from-origin.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int furthestDistanceFromOrigin(string moves) {
-        int x = 0;
-        int count = 0;  
-        
-        // Single pass to compute net and wildcards
-        for (char &ch:moves) {
-            if (ch == 'L') x++;     // 'L' increases left bias (towards negative)
-            else if (ch == 'R') x--; // 'R' increases right bias (towards positive)
-            else count++;           // '_' can go either way
+        int ans=0;
+        int count=0;
+
+        for(int i=0;i<moves.length();i++){
+            if(moves[i]=='L'){
+                ans++;
+            }else if(moves[i]=='R'){
+                ans--;
+            }else{
+                count++;
+            }
         }
-        // Max distance: all '_' align to maximize |position|
-        return max(abs(x + count), abs(x - count));  
+        return max(abs(ans+count),abs(ans-count));
     }
 };
