4d71617e4d48476bcc76b779247c238d7b17f9e5
vishal kumar <vishalkumar150206@gmail.com>
2026-05-03 18:35:57 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (97.22%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -4,8 +4,7 @@ public:
         int m=s.length();
         int n=goal.length();
 
-        if(m!=n) return false;
-        for(int count=1;count<=m;count++){
+        for(int i=0;i<=m;i++){
             rotate(s.begin(),s.begin()+1,s.end());
             if(s==goal) return true;
         }
