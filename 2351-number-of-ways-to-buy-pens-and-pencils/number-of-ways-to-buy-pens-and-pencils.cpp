aa6090dc63ffa34be415ae8dfdf1f8ad8cbae724
vishal kumar <vishalkumar150206@gmail.com>
2026-05-11 12:52:07 +0530

    Time: 64 ms (32.87%) | Memory: 39.2 MB (1.75%) - LeetSync

---
+++ 2351-number-of-ways-to-buy-pens-and-pencils/number-of-ways-to-buy-pens-and-pencils.cpp
@@ -0,0 +1,6 @@
+class Solution {
+public:
+    long long waysToBuyPensPencils(int total, int cost1, int cost2) {
+    return total < 0 ? 0 : 1 + total / cost2 + waysToBuyPensPencils(total - cost1, cost1, cost2);
+}
+};
