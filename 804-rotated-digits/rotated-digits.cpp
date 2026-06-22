eb8b745c23bf428328a5dfc2a3e4449c0d4f8024
vishal kumar <vishalkumar150206@gmail.com>
2026-06-22 10:09:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (10.45%) - LeetSync

--- 804-rotated-digits/rotated-digits.cpp
+++ 804-rotated-digits/rotated-digits.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
     int rotatedDigits(int n) {
-        vector<int> dp(n + 1, 0);
-        int count = 0;
+        vector<int>dp(n+1,0);
+        int cnt=0;
 
-        for(int i = 0; i <= n; i++){
-            if(i < 10){
-                if(i == 0 || i == 1 || i == 8) dp[i] = 1;
-                else if(i == 2 || i == 5 || i == 6 || i == 9){
-                    dp[i] = 2;
-                    count++;
-                }
-                else dp[i] = 0;
+        for(int i=0;i<=n;i++){
+            if(i<10){
+            if(i==0||i==1||i==8){
+                dp[i]=1;
+            }else if(i==2||i==5||i==6||i==9){
+                dp[i]=2;
+                cnt++;
+            }else{
+                dp[i]=0;
             }
-            else{
-                int a = dp[i / 10];
-                int b = dp[i % 10];
+            }else{
+                int a=dp[i/10];
+                int b=dp[i%10];
 
-                if(a == 1 && b == 1) dp[i] = 1;
-                else if(a >= 1 && b >= 1){
-                    dp[i] = 2;
-                    count++;
+                if(a==1 && b==1) dp[i]=1;
+                else if(a>=1 && b>=1){
+                    dp[i]=2;
+                    cnt++;
+                }else{
+                    dp[i]=0;
                 }
-                else dp[i] = 0;
             }
         }
-
-        return count;
+        return cnt;
     }
 };
