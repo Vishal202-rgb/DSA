8579c55d7ea89567416315ec037ee08a3c1fdad0
vishal kumar <vishalkumar150206@gmail.com>
2026-06-26 01:03:15 +0530

    Time: 4 ms (65.74%) | Memory: 23.9 MB (59.05%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -9,7 +9,7 @@ public:
             if(res.empty()||res.back()[1]<interval[0]){
                 res.push_back(interval);
             }else{
-                res.back()[1]=max( res.back()[1],interval[1]);
+                res.back()[1]=max(res.back()[1],interval[1]);
             }
         }
         return res;
