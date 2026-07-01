b007a9e124105f2a06e083365c3e7ead4f4fb39e
vishal kumar <vishalkumar150206@gmail.com>
2026-07-01 15:58:49 +0530

    Time: 3 ms (51.02%) | Memory: 141.7 MB (6.83%) - LeetSync

--- 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.cpp
+++ 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.cpp
@@ -4,16 +4,16 @@ public:
         int m=nums1.size();
         int n=nums2.size();
         int i=0,j=0;
-        int count=0;
+        int ans=0;
 
        while(i<m && j<n){
         if(nums1[i]>nums2[j]){
             i++;
         }else{
-            count=max(count,j-i);
+            ans=max(ans,j-i);
             j++;
         }
        }
-       return count;
+       return ans;
     }
 };
