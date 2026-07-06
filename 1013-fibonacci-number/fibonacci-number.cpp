fbe76964c4749e627123b962062fcee2068bf599
vishal kumar <vishalkumar150206@gmail.com>
2026-07-07 01:11:45 +0530

    Time: 3 ms (65.80%) | Memory: 8.3 MB (10.61%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -12,7 +12,19 @@ public:
          return fib(n-1)+fib(n-2);*/
 
          //Approach-02 (Top-Down Approach)
-         vector<int>dp(n+1,-1);
-         return fibnoacci(n,dp);
+         /*vector<int>dp(n+1,-1);
+         return fibnoacci(n,dp);*/
+
+         //Approach-03 (Bottom-Up Approach)
+         if(n<=1) return n;
+         vector<int>dp(n+1);
+
+         dp[0]=0;
+         dp[1]=1;
+
+         for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+         }
+         return dp[n];
     }
 };
