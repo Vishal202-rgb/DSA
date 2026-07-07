02be9c584d12dfdf7586a989dd5549bc42ee7e57
vishal kumar <vishalkumar150206@gmail.com>
2026-07-08 00:58:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (51.66%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,30 +1,12 @@
 class Solution {
 public:
+    int climb(int n,vector<int>&dp){
+        if(n<=3) return n;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=climb(n-1,dp)+climb(n-2,dp);
+    }
     int climbStairs(int n) {
-        //Approch-01 (Recursion)->they gives TLE
-        /*if(n==0 || n==1) return 1;
-
-        return climbStairs(n-1)+climbStairs(n-2);*/
-
-        //Approach-02 (Boottom-Up Approach)
-        /*if(n==0 || n==1) return 1;
-        vector<int>dp(n+1);
-        dp[0]=dp[1]=1;
-
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];*/
- 
-        //Approach-03 (Space Optimization)
-        if(n==0 || n==1) return 1;
-        int prev=1,curr=1;
-        for(int i=2;i<=n;i++){
-            int temp=curr;
-            curr=prev+curr;
-            prev=temp;
-        }
-        return curr;
-
+        vector<int>dp(n+1,-1);
+        return climb(n,dp);
     }
 };
