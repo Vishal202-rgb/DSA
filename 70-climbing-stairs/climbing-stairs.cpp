98e43603098c8f6a80d825e3948deb1e08f0e4a1
vishal kumar <vishalkumar150206@gmail.com>
2026-07-07 12:21:37 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (87.67%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -7,13 +7,24 @@ public:
         return climbStairs(n-1)+climbStairs(n-2);*/
 
         //Approach-02 (Boottom-Up Approach)
-        if(n==0 || n==1) return 1;
+        /*if(n==0 || n==1) return 1;
         vector<int>dp(n+1);
         dp[0]=dp[1]=1;
 
         for(int i=2;i<=n;i++){
             dp[i]=dp[i-1]+dp[i-2];
         }
-        return dp[n];
+        return dp[n];*/
+ 
+        //Approach-03 (Space Optimization)
+        if(n==0 || n==1) return 1;
+        int prev=1,curr=1;
+        for(int i=2;i<=n;i++){
+            int temp=curr;
+            curr=prev+curr;
+            prev=temp;
+        }
+        return curr;
+
     }
 };
