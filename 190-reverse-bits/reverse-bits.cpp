f2f84d6a1551f5766c4135f09becf55fe13f4638
vishal kumar <vishalkumar150206@gmail.com>
2026-07-08 19:00:12 +0530

    Time: 4 ms (28.52%) | Memory: 8.2 MB (45.94%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -3,14 +3,13 @@ public:
     int reverseBits(int n) {
         if (n == 0) return 0;
     
-        int result = 0;
+        int ans = 0;
         for (int i = 0; i < 32; i++) {
-            result <<= 1;   
-
-            result = (result | (n & 1));
+            ans <<= 1;   //Left shift ans by 1.
+            ans = (ans | (n & 1)); //This line copies the last bit of n into ans.
             
             n >>= 1;
         }
-        return result;
+        return ans;
     }
 };
