5761e3eb6fc8e7639f11d3c01f8f65651924fbc8
vishal kumar <vishalkumar150206@gmail.com>
2026-07-17 09:02:24 +0530

    Time: 4 ms (29.31%) | Memory: 9.3 MB (20.08%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int findTotalComb(int n,int target,vector<int>&nums,vector<vector<int>>&dp){
+    int findTotalComb(int n,int target,vector<int>&nums,vector<int>&dp){
         if(target==0) return 1;
         if(target<0) return 0;
-        if(dp[n][target]!=-1) return dp[n][target];
+        if(dp[target]!=-1) return dp[target];
         int ans=0;
         for(int j=0;j<n;j++){
             ans+=findTotalComb(n,target-nums[j],nums,dp);
         }
-        return dp[n][target]=ans;
+        return dp[target]=ans;
     }
     int combinationSum4(vector<int>& nums, int target) {
         int n=nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
+        vector<int>dp(target+1,-1);
         return findTotalComb(n,target,nums,dp);
     }
 };
