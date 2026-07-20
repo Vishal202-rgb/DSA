97034bb8b551e8faf80cfd61565932d5d4a9f9cf
vishal kumar <vishalkumar150206@gmail.com>
2026-07-20 23:50:12 +0530

    Time: 554 ms (5.02%) | Memory: 222.6 MB (32.27%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    int maximumProfit(int idx,int buy,int transiction,int n,vector<int>& prices,vector<vector<vector<int>>>&dp){
-        if(transiction==0 || idx==n) return 0;
-        if(dp[idx][buy][transiction]!=-1) return dp[idx][buy][transiction];
-        if(buy){
-            //either i can buy the stock and i will not buy the stock
-            return dp[idx][buy][transiction]=max((-prices[idx]+maximumProfit(idx+1,0,transiction,n,prices,dp)),maximumProfit(idx+1,1,transiction,n,prices,dp));
-        }else{
-            //either i cal sell the stock today or i can't sell the stock today
-            return dp[idx][buy][transiction]=max((prices[idx]+maximumProfit(idx+1,1,transiction-1,n,prices,dp)),maximumProfit(idx+1,0,transiction,n,prices,dp));
-        }
-    }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,-1)));
-        return maximumProfit(0,1,2,n,prices,dp);
+        vector<vector<int>>prev(2,vector<int>(3,0));
+
+        for(int i=n-1;i>=0;i--){
+            vector<vector<int>>curr(2,vector<int>(3,0));
+            for(int j=0;j<=1;j++){
+                for(int k=1;k<=2;k++){
+                    if(j==1){
+                        curr[j][k]=max(-prices[i]+prev[0][k],prev[1][k]);
+                    }else{
+                        curr[j][k]=max(prices[i]+prev[1][k-1],prev[0][k]);
+                    }
+                }
+            }
+            prev=curr;
+        }
+        return prev[1][2];
     }
 };
