3d1c162863671997c0569dbbcdae3d5806a19a8d
vishal kumar <vishalkumar150206@gmail.com>
2026-07-21 00:14:14 +0530

    Time: 401 ms (46.13%) | Memory: 211.3 MB (51.83%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,21 +2,19 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>>prev(2,vector<int>(3,0));
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
 
         for(int i=n-1;i>=0;i--){
-            vector<vector<int>>curr(2,vector<int>(3,0));
             for(int j=0;j<=1;j++){
                 for(int k=1;k<=2;k++){
                     if(j==1){
-                        curr[j][k]=max(-prices[i]+prev[0][k],prev[1][k]);
+                        dp[i][j][k]=max(-prices[i]+dp[i+1][0][k],dp[i+1][1][k]);
                     }else{
-                        curr[j][k]=max(prices[i]+prev[1][k-1],prev[0][k]);
+                        dp[i][j][k]=max(prices[i]+dp[i+1][1][k-1],dp[i+1][0][k]);
                     }
                 }
             }
-            prev=curr;
         }
-        return prev[1][2];
+        return dp[0][1][2];
     }
 };
