fbec579afd0a35c69f3ac57b09310f7e93159bcb
vishal kumar <vishalkumar150206@gmail.com>
2026-07-24 08:57:37 +0530

    Time: 4 ms (5.51%) | Memory: 11 MB (6.39%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,26 +1,49 @@
 class Solution {
 public:
-    bool check(int idx,int open,string &s,int n,vector<vector<int>>&dp){
-        if(idx==n) return open==0;
-        if(dp[idx][open]!=-1) return dp[idx][open];
+    // bool check(int idx,int open,string &s,int n,vector<vector<int>>&dp){
+    //     if(idx==n) return open==0;
+    //     if(dp[idx][open]!=-1) return dp[idx][open];
 
-        bool isValid=false;
-        if(s[idx]=='*'){
-            isValid |=check(idx+1,open+1,s,n,dp);//-->open ke liye
-            isValid |=check(idx+1,open,s,n,dp);
-            if(open>0){
-                isValid |=check(idx+1,open-1,s,n,dp);//-->close ke liye
-            }
-        }else if(s[idx]=='('){
-            isValid |=check(idx+1,open+1,s,n,dp);
-        }else if(open>0){
-            isValid |=check(idx+1,open-1,s,n,dp);
-        }
-        return dp[idx][open]=isValid;
-    }
+    //     bool isValid=false;
+    //     if(s[idx]=='*'){
+    //         isValid |=check(idx+1,open+1,s,n,dp);//-->open ke liye
+    //         isValid |=check(idx+1,open,s,n,dp);//-->empty ke liiye 
+    //         if(open>0){
+    //             isValid |=check(idx+1,open-1,s,n,dp);//-->close ke liye
+    //         }
+    //     }else if(s[idx]=='('){
+    //         isValid |=check(idx+1,open+1,s,n,dp);
+    //     }else if(open>0){
+    //         isValid |=check(idx+1,open-1,s,n,dp);
+    //     }
+    //     return dp[idx][open]=isValid;
+    // }
     bool checkValidString(string s) {
-        int n=s.length();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return check(0,0,s,n,dp);
+    int n = s.size();
+
+    vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+    dp[n][0] = 1;
+
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = 0; j <= n; j++) {
+            if (s[i] == '*') {
+                if (j + 1 <= n)
+                    dp[i][j] |= dp[i + 1][j + 1];
+                dp[i][j] |= dp[i + 1][j];
+                if (j > 0)
+                    dp[i][j] |= dp[i + 1][j - 1];
+            }
+            else if (s[i] == '(') {
+                if (j + 1 <= n)
+                    dp[i][j] |= dp[i + 1][j + 1];
+            }
+            else {
+                if (j > 0)
+                    dp[i][j] |= dp[i + 1][j - 1];
+            }
+        }
+    }
+    return dp[0][0];
     }
 };
