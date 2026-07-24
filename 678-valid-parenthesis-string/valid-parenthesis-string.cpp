8e3f0e9f56f1fa950ca425b108442fe4f1fb5128
vishal kumar <vishalkumar150206@gmail.com>
2026-07-24 09:00:14 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (11.93%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,49 +1,31 @@
 class Solution {
 public:
-    // bool check(int idx,int open,string &s,int n,vector<vector<int>>&dp){
-    //     if(idx==n) return open==0;
-    //     if(dp[idx][open]!=-1) return dp[idx][open];
-
-    //     bool isValid=false;
-    //     if(s[idx]=='*'){
-    //         isValid |=check(idx+1,open+1,s,n,dp);//-->open ke liye
-    //         isValid |=check(idx+1,open,s,n,dp);//-->empty ke liiye 
-    //         if(open>0){
-    //             isValid |=check(idx+1,open-1,s,n,dp);//-->close ke liye
-    //         }
-    //     }else if(s[idx]=='('){
-    //         isValid |=check(idx+1,open+1,s,n,dp);
-    //     }else if(open>0){
-    //         isValid |=check(idx+1,open-1,s,n,dp);
-    //     }
-    //     return dp[idx][open]=isValid;
-    // }
     bool checkValidString(string s) {
     int n = s.size();
+    vector<int>prev(n+1,0);
 
-    vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
-
-    dp[n][0] = 1;
-
+    prev[0] = 1;
     for (int i = n - 1; i >= 0; i--) {
+        vector<int>curr(n+1,0);
         for (int j = 0; j <= n; j++) {
             if (s[i] == '*') {
                 if (j + 1 <= n)
-                    dp[i][j] |= dp[i + 1][j + 1];
-                dp[i][j] |= dp[i + 1][j];
+                    curr[j] |= prev[j + 1];
+                curr[j] |= prev[j];
                 if (j > 0)
-                    dp[i][j] |= dp[i + 1][j - 1];
+                    curr[j] |= prev[j - 1];
             }
             else if (s[i] == '(') {
                 if (j + 1 <= n)
-                    dp[i][j] |= dp[i + 1][j + 1];
+                    curr[j] |= prev[j + 1];
             }
             else {
                 if (j > 0)
-                    dp[i][j] |= dp[i + 1][j - 1];
+                    curr[j] |= prev[j - 1];
             }
         }
+        prev=curr;
     }
-    return dp[0][0];
+    return prev[0];
     }
 };
