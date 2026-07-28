425ef6ab447fa7cbc5af9558d99bb5dbe01be155
vishal kumar <vishalkumar150206@gmail.com>
2026-07-28 09:08:55 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (98.73%) - LeetSync

--- 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.cpp
+++ 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.cpp
@@ -1,26 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
+    int countNodes(TreeNode* root){
+        if(!root) return 0;
+        return 1+countNodes(root->left)+countNodes(root->right);
+    }
+    bool dfs(TreeNode* root,int i,int totalNodes){
+        if(!root) return true;
+        if(i>totalNodes) return false;
+
+        return dfs(root->left,2*i,totalNodes) && dfs(root->right,2*i+1,totalNodes);
+    }
     bool isCompleteTree(TreeNode* root) {
-        //BFS 
-        queue<TreeNode*> que;
-        que.push(root);
+        //DFS
+        int totalNodes=countNodes(root);
         
-        bool past = false;
-        
-        while(!que.empty()) {
-            TreeNode* node = que.front();
-            que.pop();
-            
-            if(node == NULL) {
-                past = true;
-            } else {
-                if(past == true) {
-                    return false;
-                }
-                que.push(node->left);
-                que.push(node->right);
-            }
-        } 
-        return true;
+        return dfs(root,1,totalNodes);
     }
 };
