39f099e8c5cb3f90778970d374c87d13bdd7a3cd
vishal kumar <vishalkumar150206@gmail.com>
2026-07-29 22:27:38 +0530

    Time: 30 ms (5.83%) | Memory: 22.5 MB (6.54%) - LeetSync

---
+++ 139-word-break/word-break.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    unordered_set<string>st;
+    int n;
+    int t[301];
+    bool solve(int idx,string &s){
+        if(idx>=n) return true;
+        if(st.find(s.substr(idx,n-idx))!=st.end()){
+            return true;
+        }
+
+        if(t[idx]!=-1) return t[idx];
+        for(int l=1;l<=n;l++){
+            string temp=s.substr(idx,l);
+            if(st.find(temp)!=st.end() && solve(idx+l,s)){
+                return t[idx]= true;
+            }
+        }
+        return t[idx]= false;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        n=s.length();
+
+        memset(t,-1,sizeof(t));
+        for(string &w:wordDict){
+            st.insert(w);
+        }
+        return solve(0,s);
+    }
+};
