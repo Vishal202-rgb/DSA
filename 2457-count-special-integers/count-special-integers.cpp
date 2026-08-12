7d49cabfcb15f82a5795e468319baaf3ce3376bc
vishal kumar <vishalkumar150206@gmail.com>
2026-08-12 10:32:40 +0530

    Time: 7 ms (66.03%) | Memory: 8.2 MB (60.64%) - LeetSync

--- 2457-count-special-integers/count-special-integers.cpp
+++ 2457-count-special-integers/count-special-integers.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
+    int t[10][2][2][1024];
     int solve(int idx,bool tight,bool st,int mask,string &num){
         if(idx==num.size()){
             if(!st) return 0;
             return 1;
+        }
+        if(t[idx][tight][st][mask]!=-1){
+            return t[idx][tight][st][mask];
         }
         int limit=tight ? num[idx]-'0':9;
         int ans=0;
@@ -17,10 +21,11 @@ public:
                 ans+=solve(idx+1,newTight,true,mask|(1<<digit),num);
             }
         }
-        return ans;
+        return t[idx][tight][st][mask]=ans;
     }
     int countSpecialNumbers(int n) {
         string num=to_string(n);
+        memset(t,-1,sizeof(t));
         return solve(0,true,false,0,num);
     }
 };
