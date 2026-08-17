cc9217833b729850ed7f9830236e7f22d1ed309b
vishal kumar <vishalkumar150206@gmail.com>
2026-08-17 19:10:51 +0530

    Time: 624 ms (39.49%) | Memory: 366.5 MB (38.15%) - LeetSync

--- 3435-block-placement-queries/block-placement-queries.cpp
+++ 3435-block-placement-queries/block-placement-queries.cpp
@@ -2,17 +2,14 @@ class Solution {
 public:
     vector<int> segmentTree;
     int n = 50000;
-
     void constructSegmentTree() {
         segmentTree.resize(4*n, 0);
     }
-
     void updateSegTree(int idx, int val, int i, int l, int r) {
         if(l == r) {
             segmentTree[i] = val;
             return;
         }
-
         int mid = l + (r-l)/2;
 
         if(idx <= mid) {
@@ -23,33 +20,26 @@ public:
 
         segmentTree[i] = max(segmentTree[2*i+1], segmentTree[2*i+2]);
     }
-
     int querySegTree(int start, int end, int i, int l, int r) {
-        if(l > end || r < start) { //out of range node hai ye, return kar jao
+        if(l > end || r < start) {
             return 0;
         }
-
         if(l >= start && r <= end) {
             return segmentTree[i];
         }
-
         int mid = l + (r-l)/2;
 
         return max(querySegTree(start, end, 2*i+1, l, mid), querySegTree(start, end, 2*i+2, mid+1, r));
     }
-
     vector<bool> getResults(vector<vector<int>>& queries) {
         constructSegmentTree();
-
-        set<int> st; //obstacles
+        set<int> st;
         st.insert(0);
 
         vector<bool> result;
-
         for(auto& query : queries) {
             if(query[0] == 1) {
                 int x = query[1];
-
                 auto it = st.upper_bound(x);
                 int nxt = (it != st.end()) ? *it : -1;
                 int pre = *prev(it);
@@ -67,11 +57,9 @@ public:
 
                 int maxGap = querySegTree(0, pre, 0, 0, n-1);
                 int best = max(maxGap, x-pre);
-
                 result.push_back(best >= sz);
             }
         }
-
         return result;
     }
 };
