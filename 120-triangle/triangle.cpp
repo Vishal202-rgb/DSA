8b97ed3806b6e489a15fe0bc6922abf3f3fad9dd
vishal kumar <vishalkumar150206@gmail.com>
2026-08-19 20:22:45 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (26.26%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -6,7 +6,7 @@ public:
 
         for(int i=n-2;i>=0;i--){
             for(int j=0;j<=i;j++){
-                t[i][j]=t[i][j]+min(t[i+1][j],t[i+1][j+1]);
+                t[i][j]+=min(t[i+1][j],t[i+1][j+1]);
             }
         }
         return t[0][0];
