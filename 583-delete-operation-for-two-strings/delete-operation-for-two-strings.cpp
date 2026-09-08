faf4139123cc2c9f26f33578c1737ab87148a30e
vishal kumar <vishalkumar150206@gmail.com>
2026-09-08 12:58:25 +0530

    Time: 58 ms (6.95%) | Memory: 11 MB (98.59%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int t[501][501];
-    int solve(int i,int j,string &a,string &b){
-        if(i==a.size() || j==b.size()) return 0;
-
+    int canSolve(int i,int j,string &a,string &b){
+        if(i==a.size()||j==b.size()) return 0;
         if(t[i][j]!=-1) return t[i][j];
-
-        if(a[i]==b[j])
-            return t[i][j]=1+solve(i+1,j+1,a,b);
-
-        return t[i][j]=max(solve(i+1,j,a,b),solve(i,j+1,a,b));                     
+        if(a[i]==b[j]){
+            return t[i][j]=1+canSolve(i+1,j+1,a,b);
+        }
+        return t[i][j]=max(canSolve(i+1,j,a,b),canSolve(i,j+1,a,b));
     }
     int minDistance(string word1, string word2) {
         memset(t,-1,sizeof(t));
-
-        int lcs = solve(0,0,word1,word2);
-        return word1.size() + word2.size() - 2*lcs;
+        int lcs=canSolve(0,0,word1,word2);
+        return word1.size()+word2.size()-2*lcs;
     }
 };
