6a6017b263cef2004c0af0a1fcf7f231502bb003
vishal kumar <vishalkumar150206@gmail.com>
2026-09-16 10:04:42 +0530

    Time: 4 ms (4.76%) | Memory: 11.9 MB (33.26%) - LeetSync

--- 979-di-string-match/di-string-match.cpp
+++ 979-di-string-match/di-string-match.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     vector<int> diStringMatch(string s) {
         int n=s.length();
-        vector<int>ans;
+        vector<int>res;
         int i=0,j=n;
 
         for(char &ch:s){
             if(ch=='I'){
-                ans.push_back(i++);
+                res.push_back(i++);
             }else{
-                ans.push_back(j--);
+                res.push_back(j--);
             }
         }
-        ans.push_back(i);
-        return ans;
+        res.push_back(i);
+        return res;
     }
 };
